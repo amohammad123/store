ae4f1e79d568bd0dcc5faa551b1148c62dfe7b61
fb <fb_itsec313@gmail.com>
2023-04-05 15:32:32 +0430

    add store 1

---
+++ store/views.py
@@ -0,0 +1,34 @@
+import requests
+from django.shortcuts import render
+from store.models import Product
+from django.contrib.auth.models import User
+# from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from .Serializer import ProductSerializer,UserSerializer
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class ProductList(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer.data
+    lookup_field = "slug"
+
+class UserList(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
+
+
+
+
