a7137b203ba59c7ee41fc4f08b66bed0402c52da
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-06 23:36:18 +0330

    chore

--- store/Serializer.py
+++ store/Serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from store.models import Product
 from django.contrib.auth.models import User
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
