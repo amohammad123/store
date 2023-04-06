a63987d31211e396ca9568ded1a6834eaf9c03bf
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-06 23:36:41 +0330

    change path name

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,6 @@ from .views import ProductList,ProductDetail
 app_name = 'store'
 
 urlpatterns=[
-    # path('', views.index, name='index'),
-    path("",ProductList.as_view(),name="list"),
-    path("<int:pk>",ProductDetail.as_view(),name="detail"),
+    path("product/",ProductList.as_view(),name="list"),
+    path("product/<int:id>/",ProductDetail.as_view(),name="detail"),
 ]
