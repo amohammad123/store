d165772e7c30118256c56b55549aee2917821ba9
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-06 23:38:58 +0330

    remove UserSerializer

--- store/Serializer.py
+++ store/Serializer.py
@@ -1,17 +1,10 @@
-from rest_framework import serializers
 from store.models import Product
-from django.contrib.auth.models import User
+from rest_framework import serializers
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-
 
 
 
