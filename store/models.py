2a3d6e16367171f63a8a5f8c1a40e3d5f423f866
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-08 00:59:55 +0330

    add `cart`, `address`, `order`, `cartProduct`, `attribute`, `attributeValue`, `cartProductValue`, `payment` models

--- store/models.py
+++ store/models.py
@@ -31,3 +31,114 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    checkout = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'carts'
+
+    def __str__(self):
+        return self.user.username
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=255)
+    phone =models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'addresses'
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    status = models.CharField(max_length=255)
+    total = models.IntegerField()
+    # payment_id = models.CharField(max_length=255)
+    # payment_status = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'orders'
+
+    def __str__(self):
+        return self.total
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE,related_name='cart_products')
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    price = models.IntegerField()
+    quantity = models.PositiveSmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'cart_products'
+
+    def __str__(self):
+        return self.product
+    
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'attributes'
+    
+    def __str__(self):
+        return self.name
+
+class AttributeValue(models.Model):
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    value = models.CharField(max_length=255)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'attribute_values'
+    
+    def __str__(self):
+        return self.value
+    
+class CartProductValue(models.Model):
+    attribute_value = models.ForeignKey(AttributeValue, on_delete=models.CASCADE)
+    cart_product = models.ForeignKey(CartProduct, on_delete=models.CASCADE)
+    value = models.CharField(max_length=255)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'cart_product_values'
+    
+    def __str__(self):
+        return self.value
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    is_paid = models.BooleanField(default=False)
+    authority = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'payments'
+
+    def __str__(self):
+        return self.amount
