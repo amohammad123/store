79570e2ef2e8a835e735f3d5e60f88d576d4ec93
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-08 01:26:11 +0330

    chore

--- base/models.py
+++ base/models.py
@@ -3,44 +3,46 @@ from account.models import *
 
 
 class Aboutus(models.Model):
+    address = models.CharField(max_length=200, default="قم", verbose_name="آدرس")
+    phone = models.CharField(max_length=12, verbose_name="تلفن")
+    email = models.EmailField(max_length=50, verbose_name="ایمبل")
+
     class Meta:
         db_table = 'aboutus'
         verbose_name = "درباره ما"
         verbose_name_plural = "درباره ما"
-
-    address = models.CharField(max_length=200, default="قم", verbose_name="آدرس")
-    phone = models.CharField(max_length=12, verbose_name="تلفن")
-    email = models.EmailField(max_length=50, verbose_name="ایمبل")
 
     def __str__(self):
         return 'شماره {}'.format(self.id)
 
 
 class Contactus(models.Model):
+    title = models.CharField(max_length=30, verbose_name="موضوع")
+    email = models.EmailField(max_length=50, verbose_name="ایمبل")
+    context = models.TextField(verbose_name='پیام')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     class Meta:
         db_table = 'contactus'
         verbose_name = "ارتباط با ما"
         verbose_name_plural = "ارتباط با ما"
-
-    title = models.CharField(max_length=30, verbose_name="موضوع")
-    email = models.EmailField(max_length=50, verbose_name="ایمبل")
-    context = models.TextField(verbose_name='پیام')
-    created_at = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Ticket(models.Model):  # ticket title
+    profile = models.ForeignKey(Profile, on_delete=models.PROTECT, null=True, verbose_name="کاربر")
+    title = models.CharField(max_length=30, verbose_name="موضوع")
+    message = models.TextField(verbose_name="پیام")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     class Meta:
         db_table = 'tickets'
         verbose_name = "تیکت"
         verbose_name_plural = "تیکت"
-
-    profile = models.ForeignKey(Profile, on_delete=models.PROTECT, null=True, verbose_name="کاربر")
-    title = models.CharField(max_length=30, verbose_name="موضوع")
-    message = models.TextField(verbose_name="پیام")
-    created_at = models.DateField(null=True, blank=True)
 
     # is admin
     def __str__(self):
@@ -49,11 +51,6 @@ class Ticket(models.Model):  # ticket title
 
 
 class Images(models.Model):
-    class Meta:
-        db_table = 'base_images'
-        verbose_name = "تصاویر"
-        verbose_name_plural = "تصاویر"
-
     image = models.ImageField(upload_to='Mainimages/', verbose_name="عکس")
     description = models.CharField(max_length=50, verbose_name="توضیحات", null=True, blank=True)
 
@@ -66,29 +63,30 @@ class Images(models.Model):
     title = models.IntegerField(choices=title_choices, null=True, blank=True, verbose_name="کاربرد")
     pub_date = models.DateTimeField(null=True, blank=True, verbose_name="زمان انتشار")
 
+    class Meta:
+        db_table = 'base_images'
+        verbose_name = "تصاویر"
+        verbose_name_plural = "تصاویر"
+
+
     def __str__(self):
         return self.title.__str__()
 
 
 class Socialmedia(models.Model):
+    name = models.CharField(max_length=20, verbose_name="نام")
+    link = models.URLField(max_length=200, unique=True, verbose_name="لینک")
+
     class Meta:
         db_table = 'socialmedias'
         verbose_name = "رسانه"
         verbose_name_plural = "رسانه"
-
-    name = models.CharField(max_length=20, verbose_name="نام")
-    link = models.URLField(max_length=200, unique=True, verbose_name="لینک")
 
     def __str__(self):
         return self.name
 
 
 class HomeSetting(models.Model):
-    class Meta:
-        db_table = 'home_settings'
-        verbose_name = "صفحه اصلی"
-        verbose_name_plural = "صفحه اصلی"
-
     slogan = models.CharField(max_length=300, verbose_name="شعار")
     superiority = models.TextField(null=True, blank=True, verbose_name="برتری")
     image = models.OneToOneField(Images, on_delete=models.PROTECT, verbose_name="تصاویر", related_name="photos",
@@ -98,6 +96,11 @@ class HomeSetting(models.Model):
     sllider = models.ManyToManyField(Images, verbose_name="اسلایدر")
     logo = models.ForeignKey(Images, verbose_name="عکس لوگو", on_delete=models.PROTECT, related_name="logo",
                              null=True)
+
+    class Meta:
+        db_table = 'home_settings'
+        verbose_name = "صفحه اصلی"
+        verbose_name_plural = "صفحه اصلی"
 
     def __str__(self):
         return 'شماره {}'.format(self.id)
