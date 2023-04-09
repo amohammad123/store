e6382e8d7e1344bd86b52446d497b483558da724
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-09 11:26:36 +0330

    remove base app

--- base/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class HomeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = HomeSetting
-        fields = '__all__'
