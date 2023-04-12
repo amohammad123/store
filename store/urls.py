0d27935b740b22128aa9348ae8643fc9b3a0f105
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-13 01:29:45 +0330

    add store urls

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from store.views import *
 
 urlpatterns=[
-    path("category/",CategoryList.as_view(),name="list"),
-    path("product/",ProductList.as_view(),name="list"),
-    path("product/<int:id>/",ProductDetail.as_view(),name="detail"),
+    path("store/category/",CategoryList.as_view(),name="list"),
+    path("store/product/",ProductList.as_view(),name="list"),
+    path("store/product/<int:id>/",ProductDetail.as_view(),name="detail"),
+    path("checkout/cart/", CartListCreate.as_view(),name="list"),
+    path("checkout/cart<int:id>/", CartDetail.as_view(), name="detail"),
+    path("payment/", OrderListCreate.as_view(), name="list"),
+    path("payment/verify/", PaymentVerifyCreate.as_view(), name="list"),
+    path("order/<int:id>/", OrderDetail.as_view(), name="detail")
 ]
