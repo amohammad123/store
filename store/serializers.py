cc8c64d07d49884c076e85ad6cd7120902219992
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-13 01:24:03 +0330

    add `CartListCreate` & `CartDetail` classes

--- store/serializers.py
+++ store/serializers.py
@@ -3,15 +3,33 @@ from rest_framework import serializers
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = "__all__"
-        
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = "__all__"
+  class Meta:
+    model = Category
+    fields = '__all__'
 
 
 
+
+class CartProductSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = CartProduct
+    fields = ['id', 'price', 'quantity', 'created_at', 'updated_at']
+
+
+class CartSerializer(serializers.ModelSerializer):
+  # cart_products = serializers.SerializerMethodField()
+
+  def create(self, validated_data):
+    cart = Cart.objects.filter(user_id=validated_data['user']).last()
+    if not cart or cart.checkout:
+      cart = Cart.objects.create(**validated_data)
+
+    return {'id':cart.pk, 'user':cart.user, 'checkout': cart.checkout}
+  
+  class Meta:
+    model = Cart
+    fields = ['id', 'user', 'checkout']
+
+  # def get_cart_products(self, cart):
+  #   cart_products = CartProduct.objects.filter(cart_id=cart.id)
+  #   return CartProductSerializer(cart_products).data if cart_products is not None else None
--- store/views.py
+++ store/views.py
@@ -1,20 +1,70 @@
+import random
 from django.shortcuts import render
 from store.models import *
 from store.serializers import *
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import Response, status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, get_object_or_404
+# from django.db.models import Sum
 
 class CategoryList(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 class ProductList(ListCreateAPIView):
-    model = Product
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
 
 class ProductDetail(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "id"
+
+class CartListCreate(ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    # permission_classes = (IsAuthenticated,)
+     
+    def filter_queryset(self, queryset):
+        queryset = queryset.filter(user_id=1)
+        return super().filter_queryset(queryset)
+    
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        cart_id = response.data.get('id')
+        cart_item = request.data['item']
+        try:
+            product = Product.objects.get(id=cart_item['product_id'])
+            # add itme to CartProduct table
+            CartProduct.objects.create(cart_id=cart_id,product_id=product.id, price=product.price, quantity=cart_item['quantity'])
+        except Product.DoesNotExist:
+            return Response({'error': 'product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        return response
+
+class CartDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    lookup_field = 'id'
+
+    def patch(self, request, *args, **kwargs):
+        cart = super().patch(request, *args, **kwargs)
+        if cart.data.get('checkout'):
+            return Response({'error':'this shopping cart has been paid'}, status=status.HTTP_400_BAD_REQUEST)
+
+        cart_product = get_object_or_404(CartProduct, id=request.data['itme_id'],cart_id=kwargs['id'])
+        if 'increment' in request.data:
+            cart_product.quantity += 1
+            cart_product.save()
+        elif 'decrement' in request.data:
+            cart_product.quantity -= 1
+            cart_product.save()
+        return cart
+
+    def delete(self, request, *args, **kwargs):
+        cart_product = get_object_or_404(CartProduct, id=request.data['itme_id'],cart_id=kwargs['id'])
+        print('cart_product: ', cart_product)
+        cart_product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
