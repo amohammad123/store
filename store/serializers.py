abcfb12a48443e08bcecb3ac84da4492a28355ec
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-13 01:25:32 +0330

    add `ProductSerializer`

--- store/serializers.py
+++ store/serializers.py
@@ -8,6 +8,18 @@ class CategorySerializer(serializers.ModelSerializer):
     fields = '__all__'
 
 
+class ProductSerializer(serializers.ModelSerializer):
+  def create(self, validated_data):
+    if validated_data['price'] >= 1000:
+      product = Product.objects.create(**validated_data)
+      print('product: ', product)
+      return product
+    else:
+      raise serializers.ValidationError("price kamtar az 1000 toman ast")
+
+  class Meta:
+    model = Product
+    fields = '__all__'
 
 
 class CartProductSerializer(serializers.ModelSerializer):
